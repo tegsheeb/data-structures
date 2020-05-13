efbc7af0929a131802296b77079abe5472b52b83
tegshee <tegshbayar@gmail.com>
2020-05-13 12:28:05 -0700

    Implement code to pass test for Queue, Functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,24 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (someInstance.size() === 0) {
+      storage[0] = value;
+    } else {
+      var lastElementKey = _.max(Object.keys(storage));
+      storage[lastElementKey + 1] = value;
+    }
   };
 
   someInstance.dequeue = function() {
+    var firstElementkey = _.min(Object.keys(storage));
+    var dequeueEle = storage[firstElementkey];
+    delete storage[firstElementkey];
+    return dequeueEle;
   };
 
   someInstance.size = function() {
+    var keys = Object.keys(storage);
+    return keys.length;
   };
 
   return someInstance;
