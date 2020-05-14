3318160f7b957eb64d8ce7a1fe66334753687b0a
tegshee <tegshbayar@gmail.com>
2020-05-13 18:05:22 -0700

    Implement code to pass tests for stack, ES6 pattern

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,31 @@
 class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  constructor() {
+  constructor(storage) {
+    this.storage = {};
   }
+
+  push( value) {
+    if (this.size() === 0) {
+      this.storage[0] = value;
+    } else {
+      var lastEleKey = _.max(Object.keys(this.storage));
+      this.storage[lastEleKey + 1] = value;
+    }
+  }
+
+  pop() {
+    var lastEleKey = _.max(Object.keys(this.storage));
+    var popEle = this.storage[lastEleKey];
+    delete this.storage[lastEleKey];
+    return popEle;
+
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
+
+
 
 }
