0f6c5f121402c19b957074ae6db4abd2529f110b
tegshee <tegshbayar@gmail.com>
2020-05-15 10:18:29 -0700

    Checkout the tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,8 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -20,4 +20,6 @@ treeMethods.contains = function(target) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+options: O(1), O(log n), O(n), O(n^2)
+
  */
