42b56c087af4c3c4fca150a5f028b73106f4e4aa
tegshee <tegshbayar@gmail.com>
2020-05-15 19:33:41 -0700

    Implement code to pass tests for Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,46 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // time complexity: constant;
+  // check if storage contains item
+  if (this.contains(item)) {
+    // if yes, return nothing
+    return;
+  } else {
+    // if no (else), add item to storage
+    this._storage[item] = item;
+  }
 };
 
 setPrototype.contains = function(item) {
+  // time complexity: constant;
+  // set._storage = {
+  // 'Susan Sarandon': 'Susan Sarandon',
+  // }
+  // check whether storage contains item
+  if (this._storage[item] !== undefined) {
+    // return true if yes
+    return true;
+  } else {
+    // return false if no
+    return false;
+  }
 };
 
 setPrototype.remove = function(item) {
+  // time complexity: constant;
+  // check if storage contains item,
+  if (this.contains(item)) {
+    // if yes, delete item
+    delete this._storage[item];
+  }
+  // else, nothing
 };
 
 /*
